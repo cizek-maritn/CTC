f3657a34d7b1c4d2d2b541c61d8fd89f141b1aa3
Martin Čížek <officialpatera6@gmail.com>
2024-04-21 17:30:19 +0200

    update #1.2
    - figured out time
    - basic concurrency
    - car struct
    
    straight up balling

--- main.go
+++ main.go
@@ -6,8 +6,18 @@ import (
 	"log"
 	"math/rand/v2"
 	"os"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
+	"unicode"
 )
+
+type Car struct {
+	Type     int
+	Created  int64
+	Finished int64
+}
 
 func loadConfig(fn string) []string {
 	//nacitani config dat
@@ -24,6 +34,7 @@ func loadConfig(fn string) []string {
 		line := scanner.Text()
 		temp := strings.Split(line, ":")
 		if len(temp[1]) > 0 {
+			temp[1] = strings.TrimLeftFunc(temp[1], unicode.IsSpace)
 			configData = append(configData, temp[1])
 		}
 	}
@@ -50,13 +61,44 @@ func randRange(min, max int) int {
 	return rand.IntN(max-min+1) + min
 }
 
+func testGo(id int, ch <-chan Car, wg *sync.WaitGroup, config []string) {
+	defer wg.Done()
+
+	for car := range ch {
+		fmt.Printf("%d: obsluhuje", id)
+		fmt.Println()
+		test, _ := time.ParseDuration(config[1])
+		test2, _ := time.ParseDuration(config[2])
+		temp := time.Duration(randRange(int(test), int(test2)))
+		time.Sleep(temp)
+		car.Finished = time.Now().UnixMilli()
+		fmt.Printf("%d: skoncila s casem: %s", id, temp)
+		fmt.Println()
+	}
+}
+
 func main() {
 	config := loadConfig("config.txt")
 
-	fmt.Println(randRange(1, 5))
-	fmt.Println((randRange(1, 5)))
-	fmt.Println((randRange(1, 5)))
+	//s = stations
+	s, _ := strconv.Atoi(config[3])
+	var wg sync.WaitGroup
+	carChan := make(chan Car)
 
+	for i := 0; i < s; i++ {
+		wg.Add(1)
+		go testGo(i, carChan, &wg, config)
+	}
+
+	for c := 0; c < 5; c++ {
+		t := time.Now().UnixMilli()
+		fmt.Println(t)
+		car := Car{1, t, t}
+		carChan <- car
+	}
+
+	close(carChan)
+	wg.Wait()
 	save(config, "results.txt")
 
 	fmt.Println("done")
